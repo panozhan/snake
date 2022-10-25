bec02bfd11bdf4cd375b627c546719832279fd93
Alex Pan <alexpaofficial@gmail.com>
2022-10-24 18:31:22 -0700

    remove solution

--- src/game.js
+++ src/game.js
@@ -32,6 +32,14 @@ class Game {
         this.handleTimePass();
     }
 
+    getTileColor_(i,j) {
+        if ((i % 2 === 0 && j % 2 === 1) || (i % 2 === 1 && j % 2 === 0)) {
+            return Game.TILE_COLOR.light; 
+        } else {
+            return Game.TILE_COLOR.dark; 
+        }
+    }
+
     /**
      * Creates a root element that has {dimension} number of rows, where each row i has id 'row-{i}'.
      * Each row has {dimension} number of tiles, where each tile j in row i has id 'row-{i}-{j}'
@@ -43,7 +51,32 @@ class Game {
      * @returns {!Element}
      */
     getRootNodeForDimension() {
-        
+        if (this.root_ !== undefined) {
+            return this.root_
+        }
+        this.root_ = document.createElement('div');
+        this.root_.id = 'root';
+
+        for (let i = 0; i < this.dimension_; ++i) {
+            const rowNode = document.createElement('div');
+            rowNode.id = `row-${i}`;
+            rowNode.className = 'row';
+            for (let j = 0; j < this.dimension_; ++j) {
+                const tile = document.createElement('div');
+                tile.id = `tile-${i}-${j}`;
+                let className = ['board-element'];
+                className.push(this.getTileColor_(i,j) === Game.TILE_COLOR.dark ? 'dark-tile' : 'light-tile');
+                tile.className = className.join(' ');
+                rowNode.appendChild(tile);           
+            }
+            this.root_.appendChild(rowNode);
+        }
+        this.drawSnake_();
+        return this.root_;
+    }
+
+    getTile_(row, col) {
+        return this.root_.children.namedItem(`row-${row}`).children.namedItem(`tile-${row}-${col}`);
     }
 
     /**
