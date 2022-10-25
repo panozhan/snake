a5a48e7efa3664f55189612f42717fe9383bfc1f
Alex Pan <alexpaofficial@gmail.com>
2022-10-24 18:27:01 -0700

    checkpoint one solution

--- src/game.js
+++ src/game.js
@@ -1,13 +1,6 @@
 const {RenderingInfo, Snake} = require("./snake");
 
 class Game {
-    static DIRECTION =  {
-        UP: 0,
-        RIGHT: 1,
-        DOWN: 2,
-        LEFT: 3
-    }
-
     static TILE_COLOR = {
         light: 0,
         dark:1
@@ -20,76 +13,24 @@ class Game {
         this.dimension_ = dimension;
         this.speed_ = speed;
         this.snake_ = new Snake(dimension);
+
+        this.countMoved_ = 0;
     }
 
-    getFoodPosition_() {
-        let row, col;
-        do {
-            row = Math.floor(this.dimension_ * Math.random());
-            col = Math.floor(this.dimension_ * Math.random());
-        } while (this.snake_.collidesWithSnake(row, col));
-
-        this.foodRow_ = row;
-        this.foodCol_ = col;
-        
-    }
-
-    handleTimePass_() {
-        this.snake_.moveForwardOne();
-        if (!this.snake_.isGameOver()) {
-            const headPosition = this.snake_.getHeadPosition();
-            if (headPosition[0] === this.foodRow_ && headPosition[1] === this.foodCol_) {
-                this.snake_.addOneLength();
-                this.drawFood();
-            }
+    handleTimePass() {
+        if (this.countMoved_ < 7) {
+            this.snake_.moveForwardOne();
             this.drawSnake_();
-            this.snake_.allowDirectionChange();
+            this.countMoved_++;
             setTimeout(() => {
-                this.handleTimePass_();
+                this.handleTimePass();
             }, this.speed_);
         }
     }
 
-    /**
-     * Deletes any food element currently on the view.
-     * Then, creates a new DIV for the a new food element in a position
-     * that is not colliding with the snake
-     */
-    drawFood_() {
-        this.getFoodPosition_();
-        const currentFood = this.root_.getElementsByClassName('food');
-        if (currentFood.length !== 0) {
-            currentFood.item(0).remove();
-        }
-        const tile = this.getTile_(this.foodRow_, this.foodCol_);
-        const food = document.createElement('div');
-        food.className = 'food';
-        tile.append(food);
-    }
-
-    handleInput(eventKey) {
-        switch (eventKey) {
-            case 'ArrowDown':
-                this.snake_.changeDirection(Snake.DIRECTION.DOWN);
-                break;
-            case 'ArrowLeft':
-                this.snake_.changeDirection(Snake.DIRECTION.LEFT);
-                break;
-            case 'ArrowRight':
-                this.snake_.changeDirection(Snake.DIRECTION.RIGHT);
-                break;
-            case 'ArrowUp':
-                this.snake_.changeDirection(Snake.DIRECTION.UP);
-                break
-        }
-    }
-
     startGame() {
-        document.addEventListener('keydown', (e) => {
-            this.handleInput(e.key);
-        });
         setTimeout(() => {
-            this.handleTimePass_();
+            this.handleTimePass();
         }, this.speed_);
     }
 
@@ -133,7 +74,6 @@ class Game {
             this.root_.appendChild(rowNode);
         }
         this.drawSnake_();
-        this.drawFood_();
         return this.root_;
     }
 
@@ -153,22 +93,7 @@ class Game {
         }
         this.snake_.getRenderingInfo().forEach(info => {
             const node = document.createElement('div');
-            let classList = ['snake'];
-            switch (info.type) { 
-                case RenderingInfo.Type.CORNER_DOWN:
-                    classList.push('round-corner-down');
-                    break;
-                case RenderingInfo.Type.CORNER_TOP:
-                    classList.push('round-corner-top');
-                    break;
-                case RenderingInfo.Type.CORNER_RIGHT: 
-                classList.push('round-corner-right');
-                    break;
-                case RenderingInfo.Type.CORNER_LEFT: 
-                classList.push('round-corner-left');
-                    break;
-            }
-            node.className = classList.join(' ');
+            node.className = 'snake';
             this.getTile_(info.row, info.column).append(node);
         });
     }
--- src/snake.js
+++ src/snake.js
@@ -3,22 +3,10 @@ class SnakePart {
         this.row = row;
         this.column = column;
         this.direction = direction;
-        this.pivot = null;
-        this.pivotNext = null;
         this.next = next;
     }
 
     move() {
-        if (this.pivot !== null) {
-            this.direction = this.pivot;
-            if (this.next !== null) {
-                this.next.pivotNext = this.pivot;
-            }
-        }
-        if (this.pivotNext !== null) {
-            this.pivot = this.pivotNext;
-            this.pivotNext = null;
-        }
         switch (this.direction) {
             case Snake.DIRECTION.DOWN:
                 this.row += 1;
@@ -37,17 +25,9 @@ class SnakePart {
 }
 
 class RenderingInfo {
-    static Type = {
-        CORNER_NONE: 0,
-        CORNER_TOP: 1,
-        CORNER_RIGHT: 2,
-        CORNER_DOWN: 3,
-        CORNER_LEFT: 4
-    }
-    constructor(row, column, type) {
+    constructor(row, column) {
         this.row = row;
         this.column = column;
-        this.type = type;
     }
 }
 
@@ -62,46 +42,6 @@ class Snake {
         this.tail_ = new SnakePart(Math.floor(boardDimension / 2), 1, Snake.DIRECTION.RIGHT);
         this.head_ = new SnakePart(Math.floor(boardDimension / 2), 2, Snake.DIRECTION.RIGHT, this.tail_);
         this.boardDimension_ = boardDimension;
-        this.justChangedDirection = false;
-    }
-
-    /**
-     * Returns whether or not a part of the snake is at the location indicated by
-     * the parameters. This function is used to see if we can place a food item
-     * at the row, column.
-     * @param {number} row 
-     * @param {number} column 
-     * @returns {boolean}
-     */
-    collidesWithSnake(row, column) {
-        let node = this.head_;
-        while (node !== null) {
-            if (node.row === row && node.column === column) {
-                return true;
-            }
-            node = node.next;
-        }
-        return false;
-    }
-
-    /**
-     * Returns true if the head of the snake is beyond the board limits OR
-     * if the head of the snake is hitting a different part of the snake 
-     * @returns {boolean}
-     */
-    isGameOver() {
-        if (this.head_.row === this.boardDimension_ || this.head_.row === -1 || 
-            this.head_.column === this.boardDimension_ || this.head_.column === -1) {
-            return true;
-        }
-        let node = this.head_.next;
-        while (node !== null) {
-            if (this.head_.row === node.row && this.head_.column === node.column) {
-                return true;
-            }
-            node = node.next;
-        }
-        return false;
     }
 
     /**
@@ -120,82 +60,13 @@ class Snake {
      * @returns {Array<!RenderingInfo>}
      */
     getRenderingInfo() {
-        const result = [];
-        switch (this.head_.direction) {
-            case Snake.DIRECTION.UP:
-                result.push(new RenderingInfo(this.head_.row, this.head_.column, RenderingInfo.Type.CORNER_TOP));
-                break;
-            case Snake.DIRECTION.RIGHT:
-                result.push(new RenderingInfo(this.head_.row, this.head_.column, RenderingInfo.Type.CORNER_RIGHT));
-                break;
-            case Snake.DIRECTION.DOWN:
-                result.push(new RenderingInfo(this.head_.row, this.head_.column, RenderingInfo.Type.CORNER_DOWN));
-                break;
-            case Snake.DIRECTION.LEFT:
-                result.push(new RenderingInfo(this.head_.row, this.head_.column, RenderingInfo.Type.CORNER_LEFT));
-                break; 
-        }
+        const result = [new RenderingInfo(this.head_.row, this.head_.column)];
         let node = this.head_.next;
         while (node !== null) {
-            if (node.next !== null) {
-                result.push(new RenderingInfo(node.row, node.column, RenderingInfo.Type.CORNER_NONE));
-                node = node.next;
-            } else {
-                let directionOfNextTick = node.direction;
-                if (node.pivot !== null) {
-                    directionOfNextTick = node.pivot;
-                }
-                switch (directionOfNextTick) {
-                    case Snake.DIRECTION.UP:
-                        result.push(new RenderingInfo(node.row, node.column, RenderingInfo.Type.CORNER_DOWN));
-                        break;
-                    case Snake.DIRECTION.RIGHT:
-                        result.push(new RenderingInfo(node.row, node.column, RenderingInfo.Type.CORNER_LEFT));
-                        break;
-                    case Snake.DIRECTION.DOWN:
-                        result.push(new RenderingInfo(node.row, node.column, RenderingInfo.Type.CORNER_TOP));
-                        break;
-                    case Snake.DIRECTION.LEFT:
-                        result.push(new RenderingInfo(node.row, node.column, RenderingInfo.Type.CORNER_RIGHT));
-                        break; 
-                }
-                return result;
-            }
+            result.push(new RenderingInfo(node.row, node.column));
+            node = node.next;
         }
         return result;
-    }
-
-    /**
-     * Appends a new SnakePart to the current tail. Then sets the tail
-     * variable to the new SnakePart just created
-     */
-    addOneLength() {
-        let newTailRow = this.tail_.row;
-        let newTailColumn = this.tail_.column;
-        switch (this.tail_.direction) {
-            case Snake.DIRECTION.UP:
-                newTailRow += 1;
-                break;
-            case Snake.DIRECTION.RIGHT:
-                newTailColumn -= 1;
-                break;
-            case Snake.DIRECTION.DOWN:
-                newTailRow -= 1; 
-                break;
-            case Snake.DIRECTION.LEFT:
-                newTailColumn += 1;
-                break; 
-        }
-        this.tail_.next = new SnakePart(newTailRow, newTailColumn, this.tail_.direction);
-        this.tail_ = this.tail_.next;
-    }
-
-    /**
-     * Returns [row, column] of the head of the snake
-     * @returns {Array<Number>}
-     */
-    getHeadPosition() {
-        return [this.head_.row, this.head_.column];
     }
 
     /**
@@ -209,40 +80,6 @@ class Snake {
             node.move();
             node = node.next;
         }
-    }
-
-    /**
-     * This allows changeDirection to actually change the direction of the snake
-     */
-    allowDirectionChange() {
-        this.justChangedDirection = false;
-    }
-
-    /**
-     * Changes the direction of the snake. It should prevent any further calls to this
-     * function to do anything before "allowDirectionChange" gets called again.
-     * This is so that if the user decides to spam the arrow key buttons really fast,
-     * the game would only listen to the first button press every X amount of milliseconds
-     * @param {Snake.Direction}
-     */
-    changeDirection(direction) {
-        if (this.justChangedDirection) {
-            return;
-        }
-        if (this.head_.direction === Snake.DIRECTION.LEFT && direction === Snake.DIRECTION.RIGHT) {
-            return;
-        }
-        if (this.head_.direction === Snake.DIRECTION.RIGHT && direction === Snake.DIRECTION.LEFT) {
-            return;
-        }
-        if (this.head_.direction === Snake.DIRECTION.UP && direction === Snake.DIRECTION.DOWN) {
-            return;
-        }
-        if (this.head_.direction === Snake.DIRECTION.DOWN && direction === Snake.DIRECTION.UP) {
-            return;
-        }
-        this.head_.pivot = direction;
-        this.justChangedDirection = true;
     }
 };
 
